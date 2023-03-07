76d2416b763eae8c5becfb1f99e1a29d80333a25
Khaled Allam <kaaoaa505@gmail.com>
2023-03-07 14:16:56 +0300

    Api Resource CRUD with Response http status code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\User;
+
 class UserController extends Controller
 {
     public function index()
     {
-        return "Hello from UserController index";
+        return User::all();
+    }
+
+    public function show(User $user)
+    {
+        return $user;
+    }
+
+    public function store()
+    {
+        $userFound = User::where('email', request()->input('email'))->count();
+
+        if($userFound > 0) return response("Error, user already exists", Response::HTTP_CONFLICT);
+
+        $user = User::Create([
+            'first_name' => request()->input('first_name'),
+            'last_name' => request()->input('last_name'),
+            'email' => request()->input('email'),
+            'password' => Hash::make(request()->input('password')),
+        ]);
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    public function update(User $user)
+    {
+        $user->update([
+            'first_name' => request()->input('first_name'),
+            'last_name' => request()->input('last_name'),
+            'email' => request()->input('email'),
+            'password' => Hash::make(request()->input('password')),
+        ]);
+
+        return response($user, Response::HTTP_ACCEPTED);
+    }
+
+    public function destroy(User $user)
+    {
+        $user->destroy($user->id);
+
+        return response('', Response::HTTP_NO_CONTENT);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -14,33 +13,15 @@ class User extends Authenticatable
 
     public $timestamps = true;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'first_name',
-        'last_name',
-        'email',
-        'password',
+    protected $guarded = [
+        'id',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
