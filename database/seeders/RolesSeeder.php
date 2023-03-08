a2bc46906633488b395befeaedc44e7373746739
Khaled Allam <kaaoaa505@gmail.com>
2023-03-08 16:10:59 +0300

    create 3 roles using seeder

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -9,6 +9,15 @@ class RolesSeeder extends Seeder
 {
     public function run(): void
     {
-        Role::factory(5)->create();
+        // Role::factory(5)->create();
+        Role::create([
+            'name'=> 'admin'
+        ]);
+        Role::create([
+            'name'=> 'editor'
+        ]);
+        Role::create([
+            'name'=> 'viewer'
+        ]);
     }
 }
