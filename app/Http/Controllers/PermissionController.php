2a34b7c1c4f6b23bfc8a02ea0ca46b935cd1b806
Khaled Allam <kaaoaa505@gmail.com>
2023-03-13 09:42:51 +0300

    display all permissions route

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PermissionResource;
+use App\Models\Permission;
+
+class PermissionController extends Controller
+{
+    public function index()
+    {
+        return PermissionResource::collection(Permission::all());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
@@ -17,6 +18,8 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::apiResource('users', UserController::class);
 
     Route::apiResource('roles', RoleController::class);
+
+    Route::apiResource('permissions', PermissionController::class)->only('index');
 
     Route::apiResource('products', ProductController::class);
 
